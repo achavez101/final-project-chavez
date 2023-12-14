75d8a534b96bf5f8ade6aaa939c9bf7552ae3ee5
achavez101 <amairanygchavez@gmail.com>
2023-12-13 18:51:04 -0800

    likes in local storage saving

--- app.js
+++ app.js
@@ -136,11 +136,226 @@
     window.open(link.value);
    }
 
+   // Likes
+   // Song 1 Like
+   
+   let button = document.getElementById("view-like1");
+      let clickCount1 = 0;
+      let display1 = document.getElementById("click-count1");
+      if (localStorage.getItem("clickCount1")) {
+         clickCount1 = parseInt(localStorage.getItem("clickCount1"));
+      }
+      button.addEventListener("click", function() {
+         clickCount1++;
+         if (clickCount1 < 2){
+          display1.innerHTML = clickCount1 + " like";
+        }else {
+          display1.innerHTML = clickCount1 + " likes";
+        }
+         localStorage.setItem("clickCount1", clickCount1);
+      });
+
+  // Song 2 Like
+
+  let button2 = document.getElementById("view-like2");
+      let clickCount2 = 0;
+      let display2 = document.getElementById("click-count2");
+      if (localStorage.getItem("clickCount2")) {
+        clickCount2 = parseInt(localStorage.getItem("clickCount2"));
+     }
+     button2.addEventListener("click", function() {
+        clickCount2++;
+        if (clickCount2 < 2){
+          display2.innerHTML = clickCount2 + " like";
+        }else {
+          display2.innerHTML = clickCount2 + " likes";
+        }
+        localStorage.setItem("clickCount2", clickCount2);
+     });
+
+     // Song 3 Like
+
+     let button3 = document.getElementById("view-like3");
+      let clickCount3 = 0;
+      let display3 = document.getElementById("click-count3");
+      if (localStorage.getItem("clickCount3")) {
+        clickCount3 = parseInt(localStorage.getItem("clickCount3"));
+     }
+     button3.addEventListener("click", function() {
+      clickCount3++;
+      if (clickCount3 < 2){
+        display3.innerHTML = clickCount3 + " like";
+      }else {
+        display3.innerHTML = clickCount3 + " likes";
+      }
+      localStorage.setItem("clickCount3", clickCount3);
+     });
+
+     // Song 4 Like
+
+     let button4 = document.getElementById("view-like4");
+      let clickCount4 = 0;
+      let display4 = document.getElementById("click-count4");
+      if (localStorage.getItem("clickCount4")) {
+        clickCount4 = parseInt(localStorage.getItem("clickCount4"));
+     }
+     button4.addEventListener("click", function() {
+        clickCount4++;
+        if (clickCount4 < 2){
+          display4.innerHTML = clickCount4 + " like";
+        }else {
+          display4.innerHTML = clickCount4 + " likes";
+        }
+        localStorage.setItem("clickCount4", clickCount4);
+     });
+
+     //Song 5 Like
+
+     let button5 = document.getElementById("view-like5");
+     let clickCount5 = 0;
+     let display5 = document.getElementById("click-count5");
+     if (localStorage.getItem("clickCount5")) {
+       clickCount5 = parseInt(localStorage.getItem("clickCount5"));
+    }
+    button5.addEventListener("click", function() {
+       clickCount5++;
+       if (clickCount5 < 2){
+         display5.innerHTML = clickCount5 + " like";
+       }else {
+         display5.innerHTML = clickCount5 + " likes";
+       }
+       localStorage.setItem("clickCount5", clickCount5);
+    });
+
+     //Song 6 Like
+
+     let button6 = document.getElementById("view-like6");
+     let clickCount6 = 0;
+     let display6 = document.getElementById("click-count6");
+     if (localStorage.getItem("clickCount6")) {
+       clickCount6 = parseInt(localStorage.getItem("clickCount6"));
+    }
+    button6.addEventListener("click", function() {
+       clickCount6++;
+       if (clickCount6 < 2){
+         display6.innerHTML = clickCount6 + " like";
+       }else {
+         display6.innerHTML = clickCount6 + " likes";
+       }
+       localStorage.setItem("clickCount6", clickCount6);
+    });
+
+    //Song 7 Like
+
+    let button7 = document.getElementById("view-like7");
+    let clickCount7 = 0;
+    let display7 = document.getElementById("click-count7");
+    if (localStorage.getItem("clickCount7")) {
+      clickCount7 = parseInt(localStorage.getItem("clickCount7"));
+   }
+   button7.addEventListener("click", function() {
+      clickCount7++;
+      if (clickCount7 < 2){
+        display7.innerHTML = clickCount7 + " like";
+      }else {
+        display7.innerHTML = clickCount7 + " likes";
+      }
+      localStorage.setItem("clickCount7", clickCount7);
+   });
+
+      //Song 8 Like
+
+      let button8 = document.getElementById("view-like8");
+      let clickCount8 = 0;
+      let display8 = document.getElementById("click-count8");
+      if (localStorage.getItem("clickCount8")) {
+        clickCount8 = parseInt(localStorage.getItem("clickCount8"));
+     }
+     button8.addEventListener("click", function() {
+        clickCount8++;
+        if (clickCount8 < 2){
+          display8.innerHTML = clickCount8 + " like";
+        }else {
+          display8.innerHTML = clickCount8 + " likes";
+        }
+        localStorage.setItem("clickCount8", clickCount8);
+     });
+
+      //Song 9 Like
+
+      let button9 = document.getElementById("view-like9");
+      let clickCount9 = 0;
+      let display9 = document.getElementById("click-count9");
+      if (localStorage.getItem("clickCount9")) {
+        clickCount9 = parseInt(localStorage.getItem("clickCount9"));
+    }
+    button9.addEventListener("click", function() {
+        clickCount9++;
+        if (clickCount9 < 2){
+          display9.innerHTML = clickCount9 + " like";
+        }else {
+          display9.innerHTML = clickCount9 + " likes";
+        }
+        localStorage.setItem("clickCount9", clickCount9);
+    });
+
+      //Song 10 Like
+
+      let button10 = document.getElementById("view-like10");
+      let clickCount10 = 0;
+      let display10 = document.getElementById("click-count10");
+      if (localStorage.getItem("clickCount10")) {
+        clickCount10 = parseInt(localStorage.getItem("clickCount10"));
+    }
+    button10.addEventListener("click", function() {
+        clickCount10++;
+        if (clickCount10 < 2){
+          display10.innerHTML = clickCount10 + " like";
+        }else {
+          display10.innerHTML = clickCount10 + " likes";
+        }
+        localStorage.setItem("clickCount10", clickCount10);
+    });
+
+
+
+     // can add an if else statement to help but right now, it's obstructing storage
+
+
+
+
+
+
+  //  var count = 0;
+  //  var button = document.getElementById("viewLike");
+  //  var result = document.getElementById("result");
+  //  button.onclick = function() {
+  //     count++;
+  //     result.innerHTML = count;
+  //  }
+
+
+  //  var count = 0;
+  //  var button = document.getElementById("viewLike");
+  //  var countDisplay = document.getElementById("count");
+  //  button.addEventListener("click", function() {
+  //   count++;
+  //   countDisplay.innerHTML = count;
+  //  });
+
+  // //  // Song 2
+  //  var count = 0;
+  //  var button = document.getElementById("song2");
+  //  var countDisplay = document.getElementById("count2");
+  //  button.addEventListener("click", function() {
+  //   count++;
+  //   countDisplay.innerHTML = count;
+  //  });
+
 
     // save to local storage
     
-
-
+    
 
 
     // Rating Sort
